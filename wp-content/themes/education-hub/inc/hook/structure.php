9585c2aaadf8cfab2d78fbea2d39b72aef55c842
copernicoalavista84 <fernandomangasmolteni+2@gmail.com>
2020-01-15 16:57:54 +0100

    subo estilos y añado link al header

--- wp-content/themes/education-hub/inc/hook/structure.php
+++ wp-content/themes/education-hub/inc/hook/structure.php
@@ -33,6 +33,11 @@ if ( ! function_exists( 'education_hub_head' ) ) :
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    
+    <!-- estilos -->
+    <link rel="stylesheet" href="wp-content/themes/education-hub/css/principal.css"/>
+    
+    
     <?php
 	}
 endif;
